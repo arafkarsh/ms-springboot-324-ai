6711bbf668f768a36db6f98980c8a949e297c932
Araf Karsh Hamid <araf.karsh@metamagic.in>
2024-05-16 12:08:59 +0530

    Updated the Sentiment Analyzer Code

--- src/main/java/io/fusion/air/microservice/ai/examples/demo/_10_SentimentAnalyzerExample.java
+++ src/main/java/io/fusion/air/microservice/ai/examples/demo/_10_SentimentAnalyzerExample.java
@@ -37,31 +37,34 @@ public class _10_SentimentAnalyzerExample {
         ChatLanguageModel model = new AiBeans().createChatLanguageModel();
         SentimentAnalyzer sentimentAnalyzer = AiServices.create(SentimentAnalyzer.class, model);
         String request = """
-                    It was an interesting movie. The songs were not that great though. 
-                    However, the background score and the song choreography made for 
-                    the song. Overall the experience was not that bad. """;
+                The movie was quite engaging, although the songs were somewhat lackluster. Nevertheless, the background 
+                score and choreography significantly enhanced the musical sequences, making up for the shortcomings. Overall, 
+                the experience was not that bad.""";
         Sentiment sentiment = sentimentAnalyzer.analyzeSentimentOf(request);
-        AiBeans.printResult(request, sentiment.name());
-
-        request = """
-                    The movie storyline was good. However, the direction, acting, and 
-                    cinematography pushed the audience into a state of forced sleep. """;
         boolean positive = sentimentAnalyzer.isPositive(request);
-        AiBeans.printResult(request, "is Positive Sentiment? = "+positive);
+        AiBeans.printResult(request,
+                "Rating of the movie     = "+sentiment.name() + "\n"
+                             +"Is Sentiment Positive?  = "+((positive) ? "Yes" : "No"));
 
         request = """
-                    In essence, “Bramayugam” delves into the intricate dynamics between 
-                    an autocratic ruler, oppressed citizens, and those yearning to rebel, 
-                    portraying the timeless struggle between the privileged and the 
-                    disenfranchised across various epochs and landscapes. Through a finely 
-                    crafted screenplay, stunning artistry, captivating cinematography, 
-                    evocative background score, and stellar performances by the central 
-                    characters, the film transcends mere entertainment, offering an immersive 
-                    cinematic journey that resonates deeply with its audience. """;
+                    The movie had a promising storyline, but the direction, acting, and cinematography were so over the top 
+                    that they lulled the audience into a state of forced sleep. """;
         sentiment = sentimentAnalyzer.analyzeSentimentOf(request);
-        AiBeans.printResult(request, sentiment.name());
-
         positive = sentimentAnalyzer.isPositive(request);
-        AiBeans.printResult(request, "is Positive Sentiment? = "+positive);
+        AiBeans.printResult(request,
+                "Rating of the movie     = "+sentiment.name() + "\n"
+                             +"Is Sentiment Positive?  = "+((positive) ? "Yes" : "No"));
+
+        request = """
+                    In essence, “Bramayugam” delves into the intricate dynamics between an autocratic ruler, oppressed citizens, 
+                    and those yearning to rebel, portraying the timeless struggle between the privileged and the disenfranchised 
+                    across various epochs and landscapes. Through a finely crafted screenplay, stunning artistry, captivating 
+                    cinematography, evocative background score, and stellar performances by the central  characters, the film 
+                    transcends mere entertainment, offering an immersive cinematic journey that resonates deeply with its audience. """;
+        sentiment = sentimentAnalyzer.analyzeSentimentOf(request);
+        positive = sentimentAnalyzer.isPositive(request);
+        AiBeans.printResult(request,
+                "Rating of the movie     = "+sentiment.name() + "\n"
+                             +"Is Sentiment Positive?  = "+((positive) ? "Yes" : "No"));
     }
 }
