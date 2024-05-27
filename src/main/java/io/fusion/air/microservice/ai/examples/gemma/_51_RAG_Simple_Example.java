776c81b04436916ffb8a33efebc12522bc15b845
Araf Karsh Hamid <araf.karsh@metamagic.in>
2024-05-27 15:06:37 +0530

    RAG Code Optimization across LLMs

---
+++ src/main/java/io/fusion/air/microservice/ai/examples/gemma/_51_RAG_Simple_Example.java
@@ -0,0 +1,55 @@
+/**
+ * (C) Copyright 2024 Araf Karsh Hamid
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.fusion.air.microservice.ai.examples.gemma;
+
+import dev.langchain4j.model.chat.ChatLanguageModel;
+import io.fusion.air.microservice.ai.core.assistants.Assistant;
+import io.fusion.air.microservice.ai.core.services.RAGBuilder;
+import io.fusion.air.microservice.ai.utils.AiBeans;
+import io.fusion.air.microservice.ai.utils.AiConstants;
+import io.fusion.air.microservice.ai.utils.ConsoleRunner;
+
+/**
+ * RAG Example
+ *
+ * @author: Araf Karsh Hamid
+ * @version:
+ * @date:
+ */
+public class _51_RAG_Simple_Example {
+
+    /**
+     * This example demonstrates how to implement an "Easy RAG" (Retrieval-Augmented Generation) application.
+     * By "easy" we mean that we won't dive into all the details about parsing, splitting, embedding, etc.
+     * <p>
+     */
+    public static void main(String[] args) {
+        // Create Chat Language Model Google Gemma
+        ChatLanguageModel model = AiBeans.getChatLanguageModelLlama(AiConstants.OLLAMA_GEMMA);
+        AiBeans.printModelDetails(AiConstants.LLM_OLLAMA, AiConstants.OLLAMA_GEMMA);
+        // Create Ai Assistant
+        // Setting up the Gen AI Context with Open AI LLM, and RAG
+        Assistant assistant = RAGBuilder.createCarRentalAssistantSimple(model);
+        // Start the Conversation with Ozazo Rental Service ChatBot
+        // - Hello
+        // - I am Sam. Can I cancel my reservation?
+        // - Do you have a refund policy?
+        // - I had an accident, should I pay extra?
+        // - Can you elaborate the usage policy?
+        // - No, thank you.
+        ConsoleRunner.startConversationWith(assistant, "OZAZO Car Rental Service ChatBot");
+    }
+}
---
+++ src/main/java/io/fusion/air/microservice/ai/examples/phi3/_59_RAG_HealthCare_Example.java
@@ -0,0 +1,55 @@
+/**
+ * (C) Copyright 2024 Araf Karsh Hamid
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.fusion.air.microservice.ai.examples.phi3;
+
+import dev.langchain4j.model.chat.ChatLanguageModel;
+import io.fusion.air.microservice.ai.core.assistants.HealthCareAssistant;
+import io.fusion.air.microservice.ai.core.services.RAGHealthCareService;
+import io.fusion.air.microservice.ai.utils.AiBeans;
+import io.fusion.air.microservice.ai.utils.AiConstants;
+import io.fusion.air.microservice.ai.utils.ConsoleRunner;
+
+/**
+ * RAG Health Care Diagnosis Service Example
+ *
+ * @author: Araf Karsh Hamid
+ * @version:
+ * @date:
+ */
+public class _59_RAG_HealthCare_Example {
+
+    /**
+     * This example demonstrates how to implement Health Care Diagnosis Service Example
+     */
+    public static void main(String[] args) {
+        // Create Chat Language Model Microsoft PHI - 3
+        ChatLanguageModel model = AiBeans.getChatLanguageModelLlama(AiConstants.OLLAMA_PHI_3);
+        AiBeans.printModelDetails(AiConstants.LLM_OLLAMA, AiConstants.OLLAMA_PHI_3);
+        // Create the Assistant
+        // Setting up the Gen AI Context with Open AI LLM, and RAG
+        // HealthCareAssistant assistant = RAGHealthCareBuilder.createHealthCareAssistant(model);
+        HealthCareAssistant assistant = new RAGHealthCareService(AiConstants.LLM_OLLAMA, AiConstants.OLLAMA_PHI_3);
+
+        // Start the Conversation with iCare Health Care Diagnosis Service ChatBot
+        // - Hi
+        // - I need the diagnosis history of Akiera Kiera for the past 3 years
+        // - I need the diagnosis history of Patient 300100202
+        // - I need the diagnosis history of Jane Susan Wood for the past 4 years
+        // - I need the diagnosis history of Patient 400100201
+        // - No, thank you.
+        ConsoleRunner.startConversationWithPrompts(assistant, "iCare - Health Care Hospitals");
+    }
+}
